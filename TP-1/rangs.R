8fc6b09b6fc4225252b6d6e4080b8ef65d56fc48
TERMONT Didier <Didier.TERMONT@sfpd.fgov.be>
2022-01-04 08:52:58 +0100

    complete the analyse for the rangs.csv file

--- TP-1/rangs.R
+++ TP-1/rangs.R
@@ -5,3 +5,13 @@ rangs <- read.csv(file="rangs.csv",header=TRUE,sep=";",dec=",")
 
 # [2] compute the median of Alumni
 meanAlumni <- median(rangs$Score.on.Alumni)
+
+# [3] .3 quantile of SCI
+q30SCI <- quantile(rangs$Score.on.SCI,0.3)
+
+# [4] correlation between Alumni and Award
+correlation  <- cor(rangs$Score.on.Alumni,rangs$Score.on.Award)
+
+# [5] variable with the higher variance 
+lapply(rangs[7:13],var)
+# the variable Award has the higher variance
